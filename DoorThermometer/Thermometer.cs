58a2ad02f4183eb79567d977c4cc2691329bb75b
kapsos <christoskapsogeorgos@gmail.com>
2022-08-23 16:27:05 +0300

    adding files

---
+++ DoorThermometer/Thermometer.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_UX_VIRVOU.Forms
+{
+
+    public partial class Thermometer : Form
+    {
+        private List<double> randomTemperatures = new List<double> { 36.6, 36.6, 36.6, 36.6, 36.6 };
+        //int seconds = 0;
+        public List<Label> LabelsToTransmit = new List<Label>();
+        public Thermometer()
+        {
+            InitializeComponent();
+        }
+
+        private void triggerEventIconButton_Click(object sender, EventArgs e)
+        {
+            LineLabel1.ForeColor = Color.White;
+            LineLabel2.ForeColor = Color.White;
+            LineLabel3.ForeColor = Color.White;
+            LineLabel4.ForeColor = Color.White;
+
+
+
+            doorCamPictureBox.ImageLocation = @"..\..\Resources\DoorCamImages\2.jpg";
+
+            this.tML1.ForeColor = System.Drawing.Color.FromArgb(255, 255, 255);
+            tML1.Visible = true;
+
+            tML1.Text = "Παρακαλώ πλησιάστε κοντά στην κάμερα";
+
+
+            //ColorTransitToBlackTimer.Interval = 35;
+            //ColorTransitToBlackTimer.Enabled = true;
+
+            //thermometerMessagesLabel.Text = "xxxxxxxxxxxxxxxxxx";
+            CenterLabelToParent(tML1);
+
+
+            MessageTimer.Interval = 2000;
+            MessageTimer.Enabled = true;
+
+            List<Label> labels = new List<Label> {tML1,LineLabel1,LineLabel2};
+            TransmitLabelColorToBlack(labels);
+
+            //tML1.ForeColor = Color.Black;
+
+
+
+
+            //loadingPictureBox.Visible = true;
+            sAL.Visible = false;
+
+
+        }
+
+        private void Thermometer_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+
+            this.Location = new Point(50, 150);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+
+
+            this.BackColor = Color.White;
+
+            List<Label> labels = new List<Label> { tML1, LineLabel1, LineLabel2 };
+            LabelsToTransmit.AddRange(labels);
+
+            ColorTransitToBlackTimer.Enabled = false;
+            MessageTimer.Enabled = false;
+
+            LineLabel1.ForeColor = Color.White;
+            LineLabel2.ForeColor = Color.White;
+            LineLabel3.ForeColor = Color.White;
+            LineLabel4.ForeColor = Color.White;
+
+
+            doorCamPictureBox.BorderStyle = BorderStyle.Fixed3D;
+
+            this.tML1.Anchor = (AnchorStyles.Left | AnchorStyles.Right);
+            this.tML1.AutoSize = false;
+            this.tML1.TextAlign = ContentAlignment.MiddleCenter;
+            this.tML1.Size = tMPanel1.Size;
+
+
+
+            this.sAL.Anchor = (AnchorStyles.Left | AnchorStyles.Right);
+            this.sAL.AutoSize = false;
+            this.sAL.TextAlign = ContentAlignment.MiddleCenter;
+            this.sAL.Size = tMPanel1.Size;
+            //this.suggestActionLabel.AutoSize = false;
+            //this.suggestActionLabel.TextAlign = ContentAlignment.MiddleCenter;
+
+            doorCamPictureBox.ImageLocation = @"..\..\Resources\DoorCamImages\1.jpg";
+            doorCamPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            tML1.Visible = false;
+            //thermometerMessagesLabel.MaximumSize = new Size(700, 0);
+            //thermometerMessagesLabel.AutoSize = true;
+
+            loadingPictureBox.Visible = false;
+            loadingPictureBox.ImageLocation = @"..\..\Resources\LoadingImages\loader1.gif";
+            loadingPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            sAL.Visible = false;
+            //suggestActionLabel.MaximumSize = new Size(700, 0);
+            //suggestActionLabel.AutoSize = true;
+        }
+
+
+        public double GetRandomTemperature(double minimum, double maximum)
+        {
+            if (randomTemperatures.Count > 5)
+            {
+                randomTemperatures.RemoveRange(5, 5);
+            }
+            //randomTemperatures.RemoveRange(5, 5);
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                randomTemperatures.Add(random.NextDouble() * (maximum - minimum) + minimum);
+            }
+
+            return randomTemperatures[random.Next(10)];
+        }
+
+        private void suggestActionLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CenterLabelToParent(Label label) {
+            int y = (label.Parent.Size.Width - label.Size.Width) / 2;
+            label.Location = new Point(y, label.Location.Y);
+        }
+
+        public void GetTemperature()
+        {
+            double temp = Math.Round(GetRandomTemperature(36.6, 41), 1);
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+
+            if (temp <= 36.6)
+            {
+                tML1.ForeColor = Color.Green;
+                sAL.Text = "Μπορείτε να περάσετε!";
+                CenterLabelToParent(sAL);
+                player = new System.Media.SoundPlayer(@"..\..\Resources\Audios\success-1-6297.wav");
+
+            }
+            else if (temp < 37.2)
+            {
+                tML1.ForeColor = Color.Orange;
+                sAL.Text = "Ξεκουραστείτε για 5 λεπτά και ξαναδοκιμάστε!";
+                CenterLabelToParent(sAL);
+                player = new System.Media.SoundPlayer(@"..\..\Resources\Audios\wrong-buzzer-6268.wav");
+            }
+            else if (temp < 38)
+            {
+                tML1.ForeColor = Color.Red;
+                sAL.Text = "Δεν μπορείτε να περάσετε! Καλό θα ήταν γυρίσετε σπίτι σας!";
+                CenterLabelToParent(sAL);
+                player = new System.Media.SoundPlayer(@"..\..\Resources\Audios\wrong-buzzer-6268.wav");
+            }
+            else
+            {
+                tML1.ForeColor = Color.Red;
+                sAL.Text = "Δεν μπορείτε να περάσετε, προτείνω rapid test covid19," +
+                    " το συντομότερο διαγνωστικό κεντρό βρίσκεται σε απόσταση 500 μέτρων(Νοσοκομείο Ευαγγέλισμός)!";
+                CenterLabelToParent(sAL);
+                player = new System.Media.SoundPlayer(@"..\..\Resources\Audios\wrong-buzzer-6268.wav");
+            }
+
+            List<Label> labels = new List<Label> {sAL,LineLabel3,LineLabel4};
+            TransmitLabelColorToBlack(labels);
+
+            player.Play();
+            tML1.Text = "Η θερμοκρασία σας : " + temp.ToString() + " °C";
+            //tML1.ForeColor = Color.Red;
+            CenterLabelToParent(tML1);
+
+            //TransitLabelColorToRed(tML1);
+
+            loadingPictureBox.Visible = false;
+            sAL.Visible = true;
+        }
+
+        public int ticks2 = 0;
+        private void messageTimer_Tick(object sender, EventArgs e)
+        {
+
+            if (ticks2 == 1)
+            {
+                loadingPictureBox.Visible = true;
+                tML1.Text = "Σταθείτε εδώ για τέσσερα(4) δευτερόλεπτα";
+
+                List<Label> labels = new List<Label> {tML1,LineLabel1,LineLabel2};
+                TransmitLabelColorToBlack(labels);
+            }else if (ticks2 == 3)
+            {
+                GetTemperature();
+
+                (sender as Timer).Enabled = false;
+                ticks2 = 0;
+            }
+
+            ticks2 += 1;
+        }
+
+        public void TransmitLabelColorToBlack(List<Label> labels)
+        {
+            LabelsToTransmit.Clear();
+            foreach (Label label in labels)
+            {
+                label.ForeColor = System.Drawing.Color.FromArgb(255, 255, 255);
+                LabelsToTransmit.Add(label);
+            }
+
+
+            ColorTransitToBlackTimer.Interval = 20;
+            ColorTransitToBlackTimer.Enabled = true;
+
+        }
+
+        public void TransmitLabelColorToRed(List<Label> labels)
+        {
+            LabelsToTransmit.Clear();
+            foreach (Label label in labels)
+            {
+                label.ForeColor = System.Drawing.Color.FromArgb(255, 255, 255);
+                LabelsToTransmit.Add(label);
+            }
+            //label.ForeColor = System.Drawing.Color.FromArgb(255, 255, 255);
+            //LabelsToTransmit = label;
+            ColorTransmitToRedTimer.Interval = 20;
+            ColorTransmitToRedTimer.Enabled = true;
+        }
+
+        public int ticks = 0;
+        public int r = 0;
+        public int g = 0;
+        public int b = 0;
+
+        private void ColorTransitToBlackTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (Label label in LabelsToTransmit)
+            {
+                label.ForeColor = System.Drawing.Color.FromArgb(255 - r, 255 - g, 255 - b);
+            }
+            //LabelToTrasmit.ForeColor = System.Drawing.Color.FromArgb(255-r, 255-g, 255-b);
+
+            if (r < 250)
+            {
+                r += 10;
+                g += 10;
+                b += 10;
+            }else if (r == 250)
+            {
+                r += 5;
+                g += 5;
+                b += 5;
+            }
+            else
+            {
+                r = 0;
+                g = 0;
+                b = 0;
+                ticks = 0;
+                (sender as Timer).Enabled = false;
+                //if ((sender as Timer).Enabled == false)
+                //{
+                //    MessageBox.Show("!!!!!!!!!!");
+                //}
+                //(sender as Timer).Enabled = false;
+            }
+
+            ticks += 1;
+        }
+
+
+        public int ticks3 = 0;
+        public int r3 = 0;
+        public int g3 = 0;
+        public int b3 = 0;
+        private void ColorTransmitToRedTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (Label label in LabelsToTransmit)
+            {
+                label.ForeColor = System.Drawing.Color.FromArgb(255 - r, 255 - g, 255 - b);
+            }
+            //LabelsToTrasmit.ForeColor = System.Drawing.Color.FromArgb(255 - r, 255 - g, 255 - b);
+
+            if (g < 250)
+            {
+                g += 10;
+                b += 10;
+            }
+            else if (r == 250)
+            {
+                g += 5;
+                b += 5;
+            }
+            else
+            {
+                r = 0;
+                g = 0;
+                b = 0;
+                ticks3 = 0;
+                (sender as Timer).Enabled = false;
+            }
+
+            ticks3 += 1;
+        }
+    }
+}
---
+++ DoorThermometer/Thermometer.designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace UI_UX_VIRVOU.Forms
+{
+    partial class Thermometer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sAL = new System.Windows.Forms.Label();
+            this.loadingPictureBox = new System.Windows.Forms.PictureBox();
+            this.tML1 = new System.Windows.Forms.Label();
+            this.triggerEventIconButton = new FontAwesome.Sharp.IconButton();
+            this.doorCamPictureBox = new System.Windows.Forms.PictureBox();
+            this.tMPanel1 = new System.Windows.Forms.Panel();
+            this.sApanel = new System.Windows.Forms.Panel();
+            this.LineLabel4 = new System.Windows.Forms.Label();
+            this.LineLabel3 = new System.Windows.Forms.Label();
+            this.MessageTimer = new System.Windows.Forms.Timer(this.components);
+            this.ColorTransitToBlackTimer = new System.Windows.Forms.Timer(this.components);
+            this.ColorTransmitToRedTimer = new System.Windows.Forms.Timer(this.components);
+            this.LineLabel1 = new System.Windows.Forms.Label();
+            this.LineLabel2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.loadingPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.doorCamPictureBox)).BeginInit();
+            this.tMPanel1.SuspendLayout();
+            this.sApanel.SuspendLayout();
+            this.SuspendLayout();
+            //
+            // sAL
+            //
+            this.sAL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.sAL.AutoEllipsis = true;
+            this.sAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sAL.Location = new System.Drawing.Point(3, 122);
+            this.sAL.Name = "sAL";
+            this.sAL.Size = new System.Drawing.Size(45, 48);
+            this.sAL.TabIndex = 10;
+            this.sAL.Text = "x";
+            this.sAL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.sAL.Click += new System.EventHandler(this.suggestActionLabel_Click);
+            //
+            // loadingPictureBox
+            //
+            this.loadingPictureBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.loadingPictureBox.Location = new System.Drawing.Point(343, 70);
+            this.loadingPictureBox.Name = "loadingPictureBox";
+            this.loadingPictureBox.Size = new System.Drawing.Size(212, 201);
+            this.loadingPictureBox.TabIndex = 9;
+            this.loadingPictureBox.TabStop = false;
+            //
+            // tML1
+            //
+            this.tML1.AutoSize = true;
+            this.tML1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tML1.Location = new System.Drawing.Point(3, 16);
+            this.tML1.Name = "tML1";
+            this.tML1.Size = new System.Drawing.Size(30, 32);
+            this.tML1.TabIndex = 8;
+            this.tML1.Text = "x";
+            this.tML1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            //
+            // triggerEventIconButton
+            //
+            this.triggerEventIconButton.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.triggerEventIconButton.IconColor = System.Drawing.Color.Black;
+            this.triggerEventIconButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.triggerEventIconButton.Location = new System.Drawing.Point(0, 0);
+            this.triggerEventIconButton.Name = "triggerEventIconButton";
+            this.triggerEventIconButton.Size = new System.Drawing.Size(285, 41);
+            this.triggerEventIconButton.TabIndex = 7;
+            this.triggerEventIconButton.Text = "Ενεργοποίηση Θερμομέτρου";
+            this.triggerEventIconButton.UseVisualStyleBackColor = true;
+            this.triggerEventIconButton.Click += new System.EventHandler(this.triggerEventIconButton_Click);
+            //
+            // doorCamPictureBox
+            //
+            this.doorCamPictureBox.Location = new System.Drawing.Point(915, 0);
+            this.doorCamPictureBox.Name = "doorCamPictureBox";
+            this.doorCamPictureBox.Size = new System.Drawing.Size(985, 906);
+            this.doorCamPictureBox.TabIndex = 6;
+            this.doorCamPictureBox.TabStop = false;
+            //
+            // tMPanel1
+            //
+            this.tMPanel1.Controls.Add(this.LineLabel1);
+            this.tMPanel1.Controls.Add(this.tML1);
+            this.tMPanel1.Location = new System.Drawing.Point(12, 151);
+            this.tMPanel1.Name = "tMPanel1";
+            this.tMPanel1.Size = new System.Drawing.Size(897, 209);
+            this.tMPanel1.TabIndex = 11;
+            //
+            // sApanel
+            //
+            this.sApanel.Controls.Add(this.LineLabel4);
+            this.sApanel.Controls.Add(this.loadingPictureBox);
+            this.sApanel.Controls.Add(this.sAL);
+            this.sApanel.Controls.Add(this.LineLabel3);
+            this.sApanel.Location = new System.Drawing.Point(12, 402);
+            this.sApanel.Name = "sApanel";
+            this.sApanel.Size = new System.Drawing.Size(897, 362);
+            this.sApanel.TabIndex = 12;
+            //
+            // LineLabel4
+            //
+            this.LineLabel4.Location = new System.Drawing.Point(-3, 311);
+            this.LineLabel4.Name = "LineLabel4";
+            this.LineLabel4.Size = new System.Drawing.Size(897, 23);
+            this.LineLabel4.TabIndex = 16;
+            this.LineLabel4.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "__";
+            //
+            // LineLabel3
+            //
+            this.LineLabel3.Location = new System.Drawing.Point(-3, 95);
+            this.LineLabel3.Name = "LineLabel3";
+            this.LineLabel3.Size = new System.Drawing.Size(897, 27);
+            this.LineLabel3.TabIndex = 15;
+            this.LineLabel3.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "__";
+            //
+            // MessageTimer
+            //
+            this.MessageTimer.Tick += new System.EventHandler(this.messageTimer_Tick);
+            //
+            // ColorTransitToBlackTimer
+            //
+            this.ColorTransitToBlackTimer.Tick += new System.EventHandler(this.ColorTransitToBlackTimer_Tick);
+            //
+            // ColorTransmitToRedTimer
+            //
+            this.ColorTransmitToRedTimer.Tick += new System.EventHandler(this.ColorTransmitToRedTimer_Tick);
+            //
+            // LineLabel1
+            //
+            this.LineLabel1.Location = new System.Drawing.Point(-3, 0);
+            this.LineLabel1.Name = "LineLabel1";
+            this.LineLabel1.Size = new System.Drawing.Size(897, 23);
+            this.LineLabel1.TabIndex = 13;
+            this.LineLabel1.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "__\r\n";
+            //
+            // LineLabel2
+            //
+            this.LineLabel2.Location = new System.Drawing.Point(9, 350);
+            this.LineLabel2.Name = "LineLabel2";
+            this.LineLabel2.Size = new System.Drawing.Size(897, 23);
+            this.LineLabel2.TabIndex = 14;
+            this.LineLabel2.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "__";
+            //
+            // Thermometer
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 907);
+            this.Controls.Add(this.sApanel);
+            this.Controls.Add(this.LineLabel2);
+            this.Controls.Add(this.tMPanel1);
+            this.Controls.Add(this.triggerEventIconButton);
+            this.Controls.Add(this.doorCamPictureBox);
+            this.Name = "Thermometer";
+            this.Text = "Thermometer";
+            this.Load += new System.EventHandler(this.Thermometer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.loadingPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.doorCamPictureBox)).EndInit();
+            this.tMPanel1.ResumeLayout(false);
+            this.tMPanel1.PerformLayout();
+            this.sApanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sAL;
+        private System.Windows.Forms.PictureBox loadingPictureBox;
+        private System.Windows.Forms.Label tML1;
+        private FontAwesome.Sharp.IconButton triggerEventIconButton;
+        private System.Windows.Forms.PictureBox doorCamPictureBox;
+        private System.Windows.Forms.Panel tMPanel1;
+        private System.Windows.Forms.Panel sApanel;
+        private System.Windows.Forms.Timer MessageTimer;
+        private System.Windows.Forms.Timer ColorTransitToBlackTimer;
+        private System.Windows.Forms.Timer ColorTransmitToRedTimer;
+        private System.Windows.Forms.Label LineLabel4;
+        private System.Windows.Forms.Label LineLabel3;
+        private System.Windows.Forms.Label LineLabel1;
+        private System.Windows.Forms.Label LineLabel2;
+    }
+}
